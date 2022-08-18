e98088079362fdafb656bf25bec2c4c6f610589e
David <davidongaroh@gmail.com>
2022-08-18 19:42:04 +0300

    commit tree 49c727e336ecfa2195049f240a9af05d0e577959

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
-end
+class Student < User
+    attr_reader :knowledge
+  
+    def initialize
+      @knowledge = []
+    end
+  
+    def learn(knowledge)
+      @knowledge << knowledge
+    end
+    def knowledge
+        @knowledge
+    end
+  
+  end
